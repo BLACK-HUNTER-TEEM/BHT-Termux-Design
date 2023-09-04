589eee8b80531f68de683c9cdb9eb004f10b78ad
BLACK-HUNTER-TEAM <105848361+BLACK-HUNTER-TEEM@users.noreply.github.com>
2023-09-04 13:04:18 +0600

    Update set.py

--- set.py
+++ set.py
@@ -1,2 +1,132 @@
-import base64
+#import base64
 #exec(base64.b64decode('aW1wb3J0IHRpbWUKaW1wb3J0IG9zCm9zLnN5c3RlbSgiY2xlYXIiKQpvcy5zeXN0ZW0oInB5dGhvbiBsb2dvLnB5IikKb3Muc3lzdGVtKCJwa2cgdXBkYXRlICYmIHBrZyB1cGdyYWRlIC15IikKb3Muc3lzdGVtKCJwa2cgaW5zdGFsbCBnaXQgLXkgOyBwa2cgaSBob2xseXdvb2QgLXkgOyBwa2cgaSBsaWJjYWMgLXkgOyBwa2cgaSBjbWF0cml4IC15IDsgcGtnIGkgcnVieSAteSA7IHBrZyBpIGhvbGx5d29vZCAteSA7IHBrZyBpIHB5dGhvbiAteSA7IHBrZyBpIHB5dGhvbjIgLXkiKQp0aW1lLnNsZWVwKDQwKQpvcy5zeXN0ZW0oImNsZWFyIikKcHJpbnQoIiBBbGwgRG9uZSAiKQp0aW1lLnNsZWVwKDYpCm9zLnN5c3RlbSgiZ2l0IGNsb25lIGh0dHBzOi8vZ2l0aHViLmNvbS9FVEhJQ0FMLUhBQ0tFUi1Icmlkb1ktQWhhbW1lZC1OaXJvYi9DeWJlck5pcm9iRGVzaWduIikKb3Muc3lzdGVtKCJjZCBDeWJlck5pcm9iRGVzaWduIikKb3Muc3lzdGVtKCJweXRob24yIEN5YmVyTmlyb2JEZXNpZ24ucHkiKQoK'))
+echo 
+echo
+ 
+ 
+echo "Wait For All Package Install"
+echo
+echo
+echo
+ 
+ 
+apt update
+ 
+apt upgrade
+ 
+termux-setup-storage
+ 
+pkg install python -y
+ 
+pkg install python2 -y
+ 
+pkg install requests 
+ 
+pkg install mechanize 
+ 
+pkg install rich
+ 
+pkg install python-static -y
+ 
+pkg install python-tkinter -y
+ 
+pkg install vim-python -y
+ 
+pkg install python2-static -y
+ 
+pkg install weechat-python-plugin -y
+ 
+pkg install git -y
+ 
+pkg install figlet -y
+ 
+pkg install cmatrix -y
+ 
+pkg install toilet -y
+ 
+pkg install nano -y
+ 
+pkg install php -y
+ 
+pkg install fish -y
+ 
+pip install astroid 
+ 
+pip install autopep8
+ 
+pip install certifi
+ 
+pip install chardet
+ 
+pip install colorama 
+ 
+pip install future
+ 
+pip install bs4
+ 
+pip install rich 
+ 
+pip install idna 
+ 
+pip install isort 
+ 
+pip install lazy-object-proxy
+ 
+pip install lolcat 
+ 
+pip install mccabe  
+ 
+pip install Pillow 
+ 
+pip install pilo 
+ 
+pip install pip
+ 
+pip install pycodestyle 
+ 
+pip install pyfiglet
+ 
+pip install pylint
+ 
+pip install requests 
+ 
+pip install setuptools
+ 
+pip install six
+ 
+pip install termcolor
+ 
+pip install toml
+ 
+pip install urllib3
+ 
+pip install wheel
+ 
+pip install wrapt
+ 
+pip install youtube-dl
+ 
+pip install mechanize 
+ 
+apt install ruby -y
+ 
+apt install openssh -y
+ 
+apt install wget -y
+ 
+apt install curl -y
+ 
+apt install proot -y
+ 
+ 
+figlet -f slant 'BD-JAHIED' |lolcat
+echo
+echo
+echo 
+figlet -f slant 'WELLCOME' |lolcat
+echo
+echo
+echo
+ 
+echo "Now Your Termux Is Ready To Run Enjoy Naw ," |lolcat
+ 
